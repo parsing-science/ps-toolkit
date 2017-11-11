b6d0fc6ac54c67a2e78181cee77fea7bca08c402
parsing-science <nicole@parsingscience.com>
2017-11-11 14:31:39 -0600

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PS_Toolkit',
-    version='2.1.0',
+    version='2.1.1',
     packages=find_packages(),
     include_package_data=False,
     zip_safe=False,
